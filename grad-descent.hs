8582f56221c34818fe66893086b7a6b0db86cb9b
Max Helman <mhh2148@columbia.edu>
2020-11-28 23:42:59 -0500

    fixed typo

---
+++ grad-descent.hs
@@ -0,0 +1,38 @@
+import System.Environment(getArgs)
+import System.Exit(die)
+
+{- |
+Module      :  <File name or $Header$ to be replaced automatically>
+Description :  <optional short text displayed on contents page>
+Copyright   :  (c) <Authors or Affiliations>
+License     :  <license>
+
+Maintainer  :  <email>
+Stability   :  unstable | experimental | provisional | stable | frozen
+Portability :  portable | non-portable (<reason>)
+
+<module description starting at first column>
+-}
+
+main :: IO ()
+main = do
+            args <- getArgs
+            filename <- case args of
+                [f] -> return (f)
+                _ -> do
+                        die $ "Usage: grad-descent <filename>"
+            csvData <- getCSVData filename
+            sequence_ $ fmap (putStrLn . show) (csvData)
+
+--Creates the 'dataframe' structure - feel free to change
+getCSVData :: FilePath -> IO [[Double]]
+getCSVData filename = do
+                        lns <- fmap lines (readFile filename)
+                        return $ map (map (\x -> read x::Double)) (map words (map rep (tail lns)))
+
+--Preprocessing for CSV files (turns all commas into spaces so we can use words)
+rep :: [Char] -> [Char]
+rep [] = []
+rep (x:xs)
+    | x == ',' = [' '] ++ (rep xs)
+    | otherwise = [x] ++ (rep xs)
