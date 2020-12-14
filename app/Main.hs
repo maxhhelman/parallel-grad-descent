ad2145037bbc011e91b4b3b6ef71d6c8ddaefaf0
Riya Chakraborty <riyamch@yahoo.com>
2020-12-13 20:20:13 -0600

    Added parallel functionality.

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,9 @@ module Main where
 import Grad
 import System.Environment(getArgs)
 import System.Exit(die)
-import Data.List(isInfixOf)
+import Data.List(isInfixOf, concat)
+import Data.Array.Repa( Array, fromListUnboxed, backpermute, foldP, computeP, toList, D, U, Source, extent )
+import Data.Array.Repa.Index
 
 {- |
 Module      :  <File name or $Header$ to be replaced automatically>
@@ -39,7 +41,7 @@ main = do
             let guess = read (head $ tail $ tail input) :: [Double]
 
 --            print $ descendTolerance csvData computeGradRowLogistic [5.1,0.1,1.1] (0.00001) (0.001::Double)
-            print $ descendTolerance csvData appLoss guess (0.00001::Double) (0.001::Double)
+            print $ descendSteps csvData appLoss guess (10000::Int) (0.0000000001::Double)
 --            print $ descendTolerance csvData computeGradRowLogistic [0.0,0.0] (0.001) (0.0001)
 --            print $ descendSteps csvData computeGradRowLogistic [0.0,0.0] (10000::Int) (0.001::Double)
 
@@ -62,23 +64,47 @@ descendTolerance csvData gradFunc guess tolerance stepSize
         maxVal = maximum $ map abs (computeGrad csvData gradFunc guess stepSize)
 
 --Actual gradient descent algorithm (uses numer of steps as stopping condition)
-descendSteps :: (Ord t1, Num t1, Num t2) => [a] -> ([t2] -> a -> [t2]) -> [t2] -> t1 -> t2 -> [t2]
+descendSteps :: [a] -> ([Double] -> a -> [Double]) -> [Double] -> Int -> Double -> [Double]
 descendSteps csvData gradFunc guess steps stepSize
     | steps < 0 = error "you can't take negative steps"
     | steps == 0 = guess
     | otherwise = descendSteps (csvData) gradFunc (zipWith (-) guess (computeGrad csvData gradFunc guess stepSize)) (steps - 1) (stepSize)
 
 --Compute the gradient
-computeGrad :: Num b => [a] -> (t -> a -> [b]) -> t -> b -> [b]
+computeGrad :: [a] -> ([Double] -> a -> [Double]) -> [Double] -> Double -> [Double]
 computeGrad csvData gradFunc params stepSize = map (* stepSize) $ specialMegaFold (fmap (gradFunc params) csvData)
 
 --Applies a fold to each column in the dataframe
-specialMegaFold :: Num c => [[c]] -> [c]
+specialMegaFold :: [[Double]] -> [Double]
 specialMegaFold [] = error "specialMegaFold not long enough"
 specialMegaFold [x] = x
 specialMegaFold xx@(x:xs:xss)
     | (length xx) == 2 = zipWith (+) x xs
     | otherwise = specialMegaFold ((zipWith (+) x xs):xss)
+
+
+parallelMegaFold :: [[Double]] -> [Double]
+parallelMegaFold [] = error "parallelMegaFold not long enough"
+parallelMegaFold [x] = x
+parallelMegaFold nested@(x:xs:xss) = practiceParallel nested
+
+practiceParallel :: [[Double]] -> [Double]
+practiceParallel nested@(x:xs:xss) = do
+                    let x = fromListUnboxed (Z :. ((length nested)::Int) :. ((length $ (head nested))::Int) ) (concat nested)
+                    let xTranspose = transpose2D x
+                    let [openZZ] = computeP xTranspose :: [Array U DIM2 Double]
+--                    newZZ <- openZZ 
+                    let finally = foldP (+) 0 openZZ
+                    moo <- finally
+                    toList moo
+
+
+transpose2D :: (Source r e) => Array r DIM2 e -> Array D DIM2 e
+transpose2D a = backpermute (swap e) swap a
+     where
+       e = extent a
+       swap (Z :. i :. j) = Z :. j :. i
+
 
 --Creates the 'dataframe' structure - feel free to change (O(n) lookup time is a problem)
 getCSVData :: FilePath -> IO [[Double]]
