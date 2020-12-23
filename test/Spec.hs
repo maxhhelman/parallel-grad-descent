dc85d13edf7428d082cbf3cd633d9258b3c245b4
Riya Chakraborty <riyamch@yahoo.com>
2020-12-22 21:01:40 -0600

    Added unit tests that display time of seq vs par operations

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,67 @@
+import Test.Hspec
+import Grad
+import System.TimeIt
+
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+  describe "Testing Gradient Descent" $ do
+    it "Parallel - 100000 rows" $ do
+        csvData <- getCSVData "data/test-5.csv"
+        output <- timeItT $ (descendSteps "parallel" csvData computeGradRowLinear [0,0] (1000::Int) (0.00000000000000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(300.0::Double))
+
+
+    it "Parallel - 10000 rows" $ do
+        csvData <- getCSVData "data/test-4.csv"
+        output <- timeItT $ (descendSteps "parallel" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000000000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(10.0::Double))
+
+
+    it "Sequential - 10000 rows" $ do
+        csvData <- getCSVData "data/test-4.csv"
+        output <- timeItT $ (descendSteps "sequential" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000000000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(300.0::Double))
+
+
+    it "Parallel - 1000 rows" $ do
+        csvData <- getCSVData "data/test-3.csv"
+        output <- timeItT $ (descendSteps "parallel" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(10.0::Double))
+
+
+    it "Sequential - 1000 rows" $ do
+        csvData <- getCSVData "data/test-3.csv"
+        output <- timeItT $ (descendSteps "sequential" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(300.0::Double))
+
+
+    it "Parallel - 100 rows" $ do
+        csvData <- getCSVData "data/test-2.csv"
+        output <- timeItT $ (descendSteps "parallel" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(10.0::Double))
+
+
+    it "Sequential - 100 rows" $ do
+        csvData <- getCSVData "data/test-2.csv"
+        output <- timeItT $ (descendSteps "sequential" csvData computeGradRowLinear [0,0] (1000::Int) (0.0000001::Double)) `seq` return ()
+        let computeTime = fst output
+        print $ computeTime
+        computeTime `shouldSatisfy` (<=(10.0::Double))
+
+
+
